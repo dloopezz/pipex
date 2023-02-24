d49785d3d596d1201e2cfe997afd567539701c9b
dlopez-s <dlopez-s@student.42.com>
2023-02-24 17:52:58 +0100

    .

--- main.c
+++ main.c
@@ -5,11 +5,20 @@ void    pipex(char *argv[], int fd1, int fd2, char env[])
 	/* pid_t data type stands for process identification and it is used to represent process ids. 
 	Whenever, we want to declare a variable that is going to be deal with the process ids we can use pid_t data type.
 	The type of pid_t data is a signed integer type (signed int or we can say int). */
-	pid_t	dad;
+	pid_t	son;
 	int	xtr[2];
 
 	pipe(xtr);
-	
+	if (xtr < 0)
+		return (-1);
+	son = fork();
+	if (son < 0)
+		return (-1);
+	if (son == 0)
+	{
+		dup2(xtr[0], STDIN_FILENO);
+		
+	}
 }
 
 int main(int argc, char *argv[], char *env[])
